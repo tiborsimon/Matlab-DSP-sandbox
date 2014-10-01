958ecddbb020540b17ae78f75f461b87c8622af4
Tibor Simon <tiborsimon90@gmail.com>
2014-10-01 22:50:53 +0200

    Renaming core files prefixes.

--- library/ds_dummytest.m
+++ library/ds_dummytest.m
@@ -1,5 +1,5 @@
-function [] = mylib_dummytest()
-%MYLIB_DUMMYTEST Function to test the installation of the library.
+function [] = ds_dummytest()
+%DS_DUMMYTEST Function to test the installation of the library.
 %   This function does nothing just prints a message to the output window.
 
 [name, version] = core_getlibrarydata();
