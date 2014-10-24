8830e254bb7620df7bda0db57b6642c7b8f65027
Tibor Simon <tiborsimon90@gmail.com>
2014-10-24 23:56:12 +0200

    Refined ds_fftconv function

--- library/dsp/ds_fftconv.m
+++ library/dsp/ds_fftconv.m
@@ -5,11 +5,20 @@ function [ out ] = ds_fftconv( in1, in2 )
 in1 = in1(:);
 in2 = in2(:);
 
+N1 = length(in1);
+N2 = length(in2);
+
+if N1 >= N2
+    N = N1;
+else
+    N = N2;
+end
+
 in1 = ds_zeropad(in1);
 in2 = ds_zeropad(in2);
 
-IN1 = fft(in1);
-IN2 = fft(in2);
+IN1 = fft(in1,N);
+IN2 = fft(in2,N);
 
 OUT = IN1.*IN2;
 
